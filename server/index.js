b8a6b197c5897e3b0d48c4ed1ef7bd251ca354a2
Stipe-Kurir <skurir00@fesb.hr>
2022-12-28 21:36:36 +0100

    posts

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
+//import env from 'react-dotenv';
+
+
+
 
 const app=express();
 
@@ -15,7 +19,10 @@ app.use(cors());
 app.use('/posts',postRoutes);
 
 const CONNECTION_URL='mongodb+srv://projekt:projekt123@cluster0.qrp4wmo.mongodb.net/?retryWrites=true&w=majority';
+//const CONNECTION_URL=process.env.REACT_APP_CON_URL;
 const PORT=process.env.PORT || 5000;
+
+
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
