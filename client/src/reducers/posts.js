a50dadab31039ee2468af65c8c3bd0399fe91216
Stipe-Kurir <skurir00@fesb.hr>
2022-12-30 21:09:11 +0100

    delete,like functionality

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -7,7 +7,11 @@ switch(action.type){
     case 'FETCH-ALL':
         return action.payload;
     case 'CREATE':
-            return [...posts, action.payload];       
+            return [...posts, action.payload];  
+    case 'DELETE':
+        return posts.filter((post) => post._id !== action.payload);
+    case 'LIKE':
+        return posts.map((post) => (post._id === action.payload._id ? action.payload : post));    
     default:
         return posts;     
 }
