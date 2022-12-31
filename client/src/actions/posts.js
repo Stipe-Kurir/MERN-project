2b885c64601d9b5e5b06dcf695021cf4137a87fc
Stipe-Kurir <skurir00@fesb.hr>
2022-12-31 10:07:26 +0100

    mobile responsiveness,action constants

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,13 +1,15 @@
 import * as api from '../api/index.js';
 
+import {FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
 
-//Action Creators
+
+
 
 export const getPosts = () => async(dispatch) => {
 
      try{
        const {data} = await api.fetchPosts(); 
-       dispatch({type:'FETCH-ALL', payload:data}); 
+       dispatch({type: FETCH_ALL, payload:data}); 
 
      }catch(error){
 
@@ -20,7 +22,7 @@ export const getPosts = () => async(dispatch) => {
 export const createPost = (post) =>async (dispatch)=>{
   try{
     const {data} = await api.createPost(post);
-    dispatch({type:'CREATE',payload: data})
+    dispatch({type: CREATE,payload: data})
 
   }
   catch(error)
@@ -33,7 +35,7 @@ export const updatePost = (id, post) => async (dispatch) => {
 
   try{
      const { data }= await api.updatePost(id, post);
-      dispatch({ type: 'UPDATE' , payload: data });
+      dispatch({ type: UPDATE , payload: data });
 
   }
   catch(error){
@@ -49,7 +51,7 @@ export const deletePost = (id) => async (dispatch) => {
 
     await api.deletePost(id);
     
-    dispatch({type: 'DELETE', payload: id});
+    dispatch({type: DELETE, payload: id});
 
   }
   catch(error)
@@ -64,7 +66,7 @@ export const likePost = (id) => async (dispatch) => {
 
   try{
      const { data }= await api.likePost(id);
-      dispatch({ type: 'LIKE' , payload: data });
+      dispatch({ type: LIKE , payload: data });
 
   }
   catch(error){
--- server/index.js
+++ server/index.js
@@ -3,16 +3,11 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
-
-
-
-console.log(process.env);
-
-
+import dotenv from 'dotenv';
 
 
 const app=express();
-
+dotenv.config();
 
 app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
@@ -21,13 +16,8 @@ app.use(cors());
 
 app.use('/posts',postRoutes);
 
-const CONNECTION_URL='mongodb+srv://projekt:projekt123@cluster0.qrp4wmo.mongodb.net/?retryWrites=true&w=majority';
-//const CONNECTION_URL=process.env.REACT_APP_CON_URL;
 const PORT=process.env.PORT || 5000;
-
-
-
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 .catch((error) => console.log(error.message));
 
