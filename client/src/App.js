d2aa0947dfb56f70b18e9b81ca3774d2096d4123
Stipe-Kurir <skurir00@fesb.hr>
2023-01-07 21:42:51 +0100

    Pagination

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
 import React from "react";
 import {Container } from '@material-ui/core';
 import Navbar from "./components/Navbar/Navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import PostDetails from './components/PostDetails/PostDetails';
 
-const App = () => (
 
+const App = () => {
+const user=JSON.parse(localStorage.getItem('profile'));
+
+return(
 <BrowserRouter>
-    <Container maxwidth="lg">
+    <Container maxwidth="xl">
       <Navbar />
       <Routes>
-        <Route path='/' exact element={<Home />} />
-        <Route path='/auth' exact element={<Auth />} />
+        <Route  path="/" element={<Navigate to='/posts' />} />
+        <Route  path="/posts"  element={<Home />}/>
+        <Route  path="/posts/search"  element={<Home />}/>
+        <Route path="/posts/:id"  element={<PostDetails />} />
+        <Route path='/auth'  element={ <Auth /> } /> 
       </Routes>
     </Container>
 </BrowserRouter>
 
    
-
 );
+};
 
 
 export default App;
